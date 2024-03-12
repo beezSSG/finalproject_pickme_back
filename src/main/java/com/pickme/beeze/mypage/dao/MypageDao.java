bf3d25ffcc93e1c86a727b280399c4591bdbef62
Mingi Kang <gi3592@gmail.com>
2024-03-12 15:43:51 +0900

    #2 Feature 마이페이지 기본베이스 추가

---
+++ src/main/java/com/pickme/beeze/mypage/dao/MypageDao.java
@@ -0,0 +1,10 @@
+package com.pickme.beeze.mypage.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface MypageDao {
+
+}
