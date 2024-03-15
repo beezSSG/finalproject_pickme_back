58edf9a43c7b7267611aedb3023cc5988b06f712
Mingi Kang <gi3592@gmail.com>
2024-03-15 09:49:49 +0900

    #14 Feature 챗봇 및 OCR 기능추가
    #14 Feature 챗봇 및 OCR 기능추가

--- src/main/java/com/pickme/beeze/login/controller/LoginController.java
+++ src/main/java/com/pickme/beeze/login/controller/LoginController.java
@@ -1,17 +1,25 @@
 package com.pickme.beeze.login.controller;
 
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.pickme.beeze.login.dto.LoginDto;
 import com.pickme.beeze.login.dto.TokenDto;
 import com.pickme.beeze.login.dto.UserDto;
 import com.pickme.beeze.login.service.LoginService;
+import com.pickme.beeze.util.NaverCloud;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("api/v1/user/*")
@@ -56,6 +64,31 @@ public class LoginController {
 	}
 	// 관리자 로그인
 
+	
+	// OCR
+	@PostMapping("/ocr")
+	public String ocr(@RequestParam("uploadfile")MultipartFile uploadfile,
+							HttpServletRequest re) throws Exception {
+		System.out.println("NaverCloudController ocr " + new Date());
+		
+		String uploadPath = re.getServletContext().getRealPath("/upload");
+		
+		// 파일명을 취득
+		String filename = uploadfile.getOriginalFilename();
+		String filepath = uploadPath + File.separator + filename;	// uploadPath + / + filename
+		
+		System.out.println(filepath);
+		
+		BufferedOutputStream os = new BufferedOutputStream(new FileOutputStream(new File(filepath)));
+		os.write(uploadfile.getBytes());
+		os.close();
+		
+		// Naver cloud
+		NaverCloud nc = new NaverCloud();
+		String response = nc.OcrProc(filepath);
+		return response;
+	}
+	
 	
 	/* TODO 토근 적용 로그인 */
 }
---
+++ src/main/java/com/pickme/beeze/util/NaverCloud.java
@@ -0,0 +1,252 @@
+package com.pickme.beeze.util;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.security.Timestamp;
+import java.util.Date;
+import java.util.UUID;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import android.util.Base64;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+
+public class NaverCloud {
+
+	@Value("${chatbot.apiURL}")
+	private String apiURL;
+	
+	@Value("${chatbot.secretKey}")
+	private String secretKey;
+	
+	public NaverCloud() {
+	}
+	
+	// TODO 챗봇
+	public String ChatBot(String voiceMessage) {
+		String chatbotMessage = "";
+
+        try {   
+            URL url = new URL(apiURL);
+
+            String message = getReqMessage(voiceMessage);
+            System.out.println("##" + message);
+
+            String encodeBase64String = makeSignature(message, secretKey);
+
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", "application/json;UTF-8");
+            con.setRequestProperty("X-NCP-CHATBOT_SIGNATURE", encodeBase64String);
+
+            // post request
+            con.setDoOutput(true);
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.write(message.getBytes("UTF-8"));
+            wr.flush();
+            wr.close();
+            int responseCode = con.getResponseCode();
+
+            BufferedReader br;
+
+            if(responseCode==200) { // Normal call
+                System.out.println(con.getResponseMessage());
+
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(
+                                con.getInputStream()));
+                String decodedString;
+                while ((decodedString = in.readLine()) != null) {
+                    chatbotMessage = decodedString;
+                }
+                //chatbotMessage = decodedString;
+                in.close();
+
+            } else {  // Error occurred
+                chatbotMessage = con.getResponseMessage();
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        System.out.println(chatbotMessage);
+    	return chatbotMessage;
+    }
+	
+    public static String makeSignature(String message, String secretKey) {
+
+        String encodeBase64String = "";
+
+        try {
+            byte[] secrete_key_bytes = secretKey.getBytes("UTF-8");
+
+            SecretKeySpec signingKey = new SecretKeySpec(secrete_key_bytes, "HmacSHA256");
+            Mac mac = Mac.getInstance("HmacSHA256");
+            mac.init(signingKey);
+
+            byte[] rawHmac = mac.doFinal(message.getBytes("UTF-8"));
+            encodeBase64String = Base64.encodeToString(rawHmac, Base64.NO_WRAP);
+
+            return encodeBase64String;
+
+        } catch (Exception e){
+            System.out.println(e);
+        }
+
+        return encodeBase64String;
+
+    }
+
+    public static String getReqMessage(String voiceMessage) {
+
+        String requestBody = "";
+
+        try {
+
+            JSONObject obj = new JSONObject();
+
+            long timestamp = new Date().getTime();
+
+            System.out.println("##"+timestamp);
+
+            obj.put("version", "v2");
+            obj.put("userId", "U47b00b58c90f8e47428af8b7bddc1231heo2");
+//=> userId is a unique code for each chat user, not a fixed value, recommend use UUID. use different id for each user could help you to split chat history for users.
+
+            obj.put("timestamp", timestamp);
+
+            JSONObject bubbles_obj = new JSONObject();
+
+            bubbles_obj.put("type", "text");
+
+            JSONObject data_obj = new JSONObject();
+            data_obj.put("description", voiceMessage);
+
+            bubbles_obj.put("type", "text");
+            bubbles_obj.put("data", data_obj);
+
+            JSONArray bubbles_array = new JSONArray();
+            bubbles_array.put(bubbles_obj);
+
+            obj.put("bubbles", bubbles_array);
+            obj.put("event", "send");
+
+            requestBody = obj.toString();
+
+        } catch (Exception e){
+            System.out.println("## Exception : " + e);
+        }
+
+        return requestBody;
+
+    }
+    
+    // TODO OCR
+	public String OcrProc(String filepath) {
+		
+		String imageFile = filepath;
+		String message = null;
+
+		try {
+			URL url = new URL(apiURL);
+			HttpURLConnection con = (HttpURLConnection)url.openConnection();
+			con.setUseCaches(false);
+			con.setDoInput(true);
+			con.setDoOutput(true);
+			con.setReadTimeout(30000);
+			con.setRequestMethod("POST");
+			String boundary = "----" + UUID.randomUUID().toString().replaceAll("-", "");
+			con.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+			con.setRequestProperty("X-OCR-SECRET", secretKey);
+
+			JSONObject json = new JSONObject();
+			json.put("version", "V2");
+			json.put("requestId", UUID.randomUUID().toString());
+			json.put("timestamp", System.currentTimeMillis());
+			JSONObject image = new JSONObject();
+			image.put("format", "jpg");
+			image.put("name", "demo");
+			JSONArray images = new JSONArray();
+			images.put(image);
+			json.put("images", images);
+			String postParams = json.toString();
+
+			con.connect();
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			long start = System.currentTimeMillis();
+			File file = new File(imageFile);
+			writeMultiPart(wr, postParams, file, boundary);
+			wr.close();
+
+			int responseCode = con.getResponseCode();
+			BufferedReader br;
+			if (responseCode == 200) {
+				br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			} else {
+				br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+			}
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+			while ((inputLine = br.readLine()) != null) {
+				response.append(inputLine);
+			}
+			br.close();
+
+			System.out.println(response);	// 결과값[json]
+			message = response.toString();
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		return message;
+	}
+
+	private static void writeMultiPart(OutputStream out, String jsonMessage, File file, String boundary) throws
+		IOException {
+		StringBuilder sb = new StringBuilder();
+		sb.append("--").append(boundary).append("\r\n");
+		sb.append("Content-Disposition:form-data; name=\"message\"\r\n\r\n");
+		sb.append(jsonMessage);
+		sb.append("\r\n");
+
+		out.write(sb.toString().getBytes("UTF-8"));
+		out.flush();
+
+		if (file != null && file.isFile()) {
+			out.write(("--" + boundary + "\r\n").getBytes("UTF-8"));
+			StringBuilder fileString = new StringBuilder();
+			fileString
+				.append("Content-Disposition:form-data; name=\"file\"; filename=");
+			fileString.append("\"" + file.getName() + "\"\r\n");
+			fileString.append("Content-Type: application/octet-stream\r\n\r\n");
+			out.write(fileString.toString().getBytes("UTF-8"));
+			out.flush();
+
+			try (FileInputStream fis = new FileInputStream(file)) {
+				byte[] buffer = new byte[8192];
+				int count;
+				while ((count = fis.read(buffer)) != -1) {
+					out.write(buffer, 0, count);
+				}
+				out.write("\r\n".getBytes());
+			}
+
+			out.write(("--" + boundary + "--\r\n").getBytes("UTF-8"));
+		}
+		out.flush();
+	}
+}
