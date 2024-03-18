910ebf57caa206c49b94a5fc200da700acf3bbbe
Mingi Kang <gi3592@gmail.com>
2024-03-18 16:49:59 +0900

    #21 Feature 스프링 시큐리티 및 jwt 구현
    #21 Feature 스프링 시큐리티 및 jwt 구현

--- src/main/java/com/pickme/beeze/login/dto/LoginDto.java
+++ src/main/java/com/pickme/beeze/login/dto/LoginDto.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Alias("LoginDto")
-public class LoginDto {
+public class LoginDto { // 토큰 사용시 점주 및 회원 통합 dto
 	
 	private int id;
 	private String email;
@@ -21,5 +21,7 @@ public class LoginDto {
 	private int point;
 	private int grade;
 	private String rdate;
+	private String role;			// customer:고객 ceo:점주 manager:관리자 이부분은 로그인 DB확인후 따로 주입하게 설정
+	private boolean loginSns;     //소셜로그인 여부 (default = false)
 
 }
