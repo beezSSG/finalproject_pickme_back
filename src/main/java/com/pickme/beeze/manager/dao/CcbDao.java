f4f7b82fc356bfa41b7dcf6cae35935f272d6045
hakiseong <ks510222@naver.com>
2024-03-19 15:32:37 +0900

    관리자 발주목록 전체보기 기능
    관리자 발주목록 전체보기 기능
    (모르고 dev에서 해버렸네요....)

--- src/main/java/com/pickme/beeze/manager/dao/CcbDao.java
+++ src/main/java/com/pickme/beeze/manager/dao/CcbDao.java
@@ -18,9 +18,11 @@ public interface CcbDao {
 	List<CcbDto> ccblist(CcbDto dto);
 	// 1:1 문의 게시글 상세보기
 	CcbDto ccbdetail(int id);
+	// 1:1 문의 관리자 댓글 보기
+	List<CcbaDto> ccbcommentlist(int ccbId);
 	// 1:1 문의 관리자 댓글 달기
-	int ccbcommentwrite(CcbaDto dto);
+	void ccbcommentwrite(CcbaDto dto);
 	// 1:1 문의 관리자 댓글 삭제
-	int ccbcommentdelete(int id);
+	void ccbcommentdelete(int id);
 	
 }
