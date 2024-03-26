b79b0e9bf903189eb3bd4d449d5d211ace7caf0d
Mingi Kang <gi3592@gmail.com>
2024-03-26 10:23:31 +0900

    #37 Fix 회원 정보 관련 수정 및 디버깅작업
    #37 Fix 회원 정보 관련 수정 및 디버깅작업

--- src/main/java/com/pickme/beeze/login/dto/LoginDto.java
+++ src/main/java/com/pickme/beeze/login/dto/LoginDto.java
@@ -19,7 +19,7 @@ public class LoginDto { // 토큰 사용시 점주 및 회원 통합 dto
 	private String address;
 	private String phone;
 	private int point;
-	private int grade;
+	private String grade;
 	private String rdate;
 	private String role;			// customer:고객 ceo:점주 manager:관리자 이부분은 로그인 DB확인후 따로 주입하게 설정
 	private boolean loginSns;     //소셜로그인 여부 (default = false)
