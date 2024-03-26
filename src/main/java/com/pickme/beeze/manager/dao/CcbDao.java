9bd550365df00e7817a9e6eb4d9c2b51be4ef371
hakiseong <ks510222@naver.com>
2024-03-26 09:33:42 +0900

    commit
    commit

--- src/main/java/com/pickme/beeze/manager/dao/CcbDao.java
+++ src/main/java/com/pickme/beeze/manager/dao/CcbDao.java
@@ -18,6 +18,8 @@ public interface CcbDao {
 	List<CcbDto> ccblist(CcbDto dto);
 	// 1:1 문의 게시글 상세보기
 	CcbDto ccbdetail(int id);
+	// 1:1 문의 게시글 지우기
+	void ccbdelete(int id);
 	// 1:1 문의 관리자 댓글 보기
 	List<CcbaDto> ccbcommentlist(int ccbId);
 	// 1:1 문의 관리자 댓글 달기
