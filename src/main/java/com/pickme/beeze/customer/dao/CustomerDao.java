119313bc5de26b8f23260df1a62ab96fca0ec0a0
wonbi Jeong <totoyo693@naver.com>
2024-04-01 15:58:51 +0900

    점주 메인 홈 화면 만들기
    점주 메인 홈 화면 만들기

--- src/main/java/com/pickme/beeze/customer/dao/CustomerDao.java
+++ src/main/java/com/pickme/beeze/customer/dao/CustomerDao.java
@@ -39,10 +39,17 @@ public interface CustomerDao {
 	// 상품 찜 추가
 	int insertZZIM(int productId, int customerId);
 	// 상품 찜 삭제
+
+
+
 	int deleteZZIM(int productId, String customerEmail);
 	
 	// 택배 예약 신청
 	int postreservation(PostDto dto);
+
+
+	int deleteZZIM(int productId, int customerId);
+
 
 
 }
