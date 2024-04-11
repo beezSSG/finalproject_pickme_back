439651779be34d942b5db7f5c7814c8943f08065
hakiseong <ks510222@naver.com>
2024-04-11 15:38:11 +0900

    #27 feat 쿠폰 생성
    #27 feat 쿠폰 생성

--- src/main/java/com/pickme/beeze/manager/dao/CouponDao.java
+++ src/main/java/com/pickme/beeze/manager/dao/CouponDao.java
@@ -3,6 +3,8 @@ package com.pickme.beeze.manager.dao;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import com.pickme.beeze.customer.dao.CustomerDao;
+import com.pickme.beeze.login.dto.CustomerDto;
 import com.pickme.beeze.manager.dto.CouponDto;
 
 @Mapper
@@ -11,6 +13,7 @@ public interface CouponDao {
 
 	// 쿠폰 생성
 	int couponcreate(CouponDto dto);
+	int countuser(CustomerDto dto);
 	// 쿠폰 삭제
 	int coupondelete(int couponNumber);
 }
