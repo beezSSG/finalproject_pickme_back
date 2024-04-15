9bc469bfb05d60e9fa0fd7c602bef43f237b7fe8
GONZOZO <149459193+GONZOZO@users.noreply.github.com>
2024-04-16 00:17:40 +0900

    #95

--- src/main/java/com/pickme/beeze/store/dto/StoreProductDto.java
+++ src/main/java/com/pickme/beeze/store/dto/StoreProductDto.java
@@ -1,5 +1,17 @@
 package com.pickme.beeze.store.dto;
 
+import org.apache.ibatis.type.Alias;
+
+import com.pickme.beeze.product.dto.ProductDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Alias("StoreProductDto")
 public class StoreProductDto {
 
 	private int id;
@@ -7,65 +19,5 @@ public class StoreProductDto {
 	private int product_id;
 	private int quantity;
 	private String exp_date;
-	
-	public StoreProductDto() {	
-	}
-
-	public StoreProductDto(int id, int store_id, int product_id, int quantity, String exp_date) {
-		super();
-		this.id = id;
-		this.store_id = store_id;
-		this.product_id = product_id;
-		this.quantity = quantity;
-		this.exp_date = exp_date;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public int getStore_id() {
-		return store_id;
-	}
-
-	public void setStore_id(int store_id) {
-		this.store_id = store_id;
-	}
-
-	public int getProduct_id() {
-		return product_id;
-	}
-
-	public void setProduct_id(int product_id) {
-		this.product_id = product_id;
-	}
-
-	public int getQuantity() {
-		return quantity;
-	}
-
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-
-	public String getExp_date() {
-		return exp_date;
-	}
-
-	public void setExp_date(String exp_date) {
-		this.exp_date = exp_date;
-	}
-
-	@Override
-	public String toString() {
-		return "StoreProductDto [id=" + id + ", store_id=" + store_id + ", product_id=" + product_id + ", quantity="
-				+ quantity + ", exp_date=" + exp_date + "]";
-	}
-	
-	
 	
 }
