56a44d3cad0fdbcd520585971bb108e2f0cabafc
hakiseong <ks510222@naver.com>
2024-04-15 13:30:51 +0900

    #27 fix 쿠폰 쿼리문 수정
    #27 fix 쿠폰 쿼리문 수정

--- src/main/java/com/pickme/beeze/mypage/dto/MypageCouponDto.java
+++ src/main/java/com/pickme/beeze/mypage/dto/MypageCouponDto.java
@@ -13,6 +13,7 @@ import lombok.NoArgsConstructor;
 public class MypageCouponDto {
 
 	private int id;
+	private String title;
 	private String content;
 	private String startDate;
 	private String endDate;
